94051011f4b9aa3bd3cbeb99c455471f045f931f
Jefferson Felipe <jefferson.felipe@esig.com.br>
2022-12-20 16:35:52 -0300

    adicionando o projeto inicial

---
+++ Projeto Erudio/src/test/java/br/com/felipjeff/projetoerudio/ProjetoErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipjeff.projetoerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
